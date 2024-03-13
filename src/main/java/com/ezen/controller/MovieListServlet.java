f9efae64fda9b4c484fcfc2eca5597d2278f30ab
ghdlrn <ghdlrn@gmail.com>
2024-03-13 17:11:17 +0900

    first commit

---
+++ src/main/java/com/ezen/controller/MovieListServlet.java
@@ -0,0 +1,40 @@
+package com.ezen.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ezen.dao.MovieDAO;
+import com.ezen.dto.MovieVO;
+
+@WebServlet("/movieList.do")
+public class MovieListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		
+//		MovieDAO mDao = MovieDAO.getInstance();
+//		List<MovieVO> list = mDao.GetMovieList();
+		List<MovieVO> list = MovieDAO.getInstance().GetMovieList();
+		
+		request.setAttribute("movieList", list);
+		String ulr = "movie/movieList.jsp";
+		
+		request.getRequestDispatcher(ulr).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int code = Integer.parseInt(request.getParameter("code"));
+		
+		MovieDAO.getInstance().deleteMoive(code);
+		
+		response.sendRedirect("movieList.do");
+	}
+
+}
